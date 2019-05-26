554c52bfbbe5255910bc00791bf152864cf49ae1
7117 <18701558590@139.com>
2019-05-26 14:21:29 +0800

    运维部分：使用ngrok本地上线

--- config/params.php
+++ config/params.php
@@ -2,9 +2,9 @@
 
 return [
     "domain" => [
-        'www' => 'http://ebk.ebk.ebk',
-        'm' => 'http://ebk.ebk.ebk/m',
-        'web' => 'http://ebk.ebk.ebk/web',
+        'www' => 'http://cba.free.idcfengye.com/',
+        'm' => 'http://cba.free.idcfengye.com/m',
+        'web' => 'http://cba.free.idcfengye.com/web',
     ],
 
     "upload" => [
@@ -12,5 +12,11 @@ return [
         'brand' => "/uploads/brand",
         'book' => "/uploads/book",
     ],
+    "weixin" => [
+        "appid" => 'wxe3004e8165bec0aa',
+        "sk" => 'acc975fdad728d7461164932ae0bbaa8',
+        "token" => 'tomsunalang689',
+        "aeskey" => 'P6PaB6bPrRzKkva5lq6kHWtYkOOlVhYq4fh1iR7LMKB'
+    ]
 
 ];
