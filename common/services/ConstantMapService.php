2d5234294525705ae7d871e7897eae065c1fc23f
7117 <18701558590@139.com>
2019-06-10 17:16:51 +0800

    财务模块：crud

--- common/services/ConstantMapService.php
+++ common/services/ConstantMapService.php
@@ -1,8 +1,9 @@
 <?php
 namespace app\common\services;
 
-class ConstantMapService
-{
+
+class ConstantMapService {
+
     public static $client_type_wechat = 1;
 
     public static $default_avatar = 'default_avatar';
@@ -28,4 +29,21 @@ class ConstantMapService
         -8 => '待支付',
         0 => '已关闭'
     ];
+
+    public static $express_status_mapping = [
+        1 => '会员已签收',
+        -6 => '已发货待签收',
+        -7 => '已付款待发货',
+        -8 => '待支付',
+        0 => '已关闭'
+    ];
+
+    public static $express_status_mapping_for_member = [
+        1  => '已签收',
+        -6 => '已发货',
+        -7 => '等待商家发货',
+        -8 => '待支付',
+        0 => '已关闭'
+    ];
+
 }
